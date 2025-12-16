d2d19a11a1b3c7a430d85080430c7a0512fd3e36
abibire <ajbibire@gmail.com>
2025-12-16 17:04:30 -0500

    bumping version number

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -97,7 +97,7 @@ android {
         applicationId = "com.andrewbibire.chessanalysis"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
     }
 
